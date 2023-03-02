60eb882901b2dbdd6bfe6bcfcace3dbcbc24e9c4
gary <garyhe.05@outlook.com>
2023-03-02 14:45:38 +0800

    fix: typo

--- routes/api/seo.ts
+++ routes/api/seo.ts
@@ -4,7 +4,7 @@ export const handler = (_req: Request, _ctx: HandlerContext): Response => {
   const data: Record<string, unknown> = {
     description:
       "he asset management division of JP Morgan Chase &amp; Co., offers a wide range of mutual funds spanning all asset classes, adhering to distinct investment styles, to build portfolios to suit various investor goals and objectives.",
-    keyword: "asset managements funds",
+    keywords: "asset managements funds",
   };
 
   const body = JSON.stringify(data);
