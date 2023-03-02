267f18864ab0eedd76a70d97adc472f7664c94ea
gary <garyhe.05@outlook.com>
2023-03-02 14:42:25 +0800

    add seo api

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,10 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/api/page.ts";
-import * as $3 from "./routes/editMode.tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/login.tsx";
+import * as $3 from "./routes/api/seo.ts";
+import * as $4 from "./routes/editMode.tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/login.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -16,9 +17,10 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/api/page.ts": $2,
-    "./routes/editMode.tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/login.tsx": $5,
+    "./routes/api/seo.ts": $3,
+    "./routes/editMode.tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/login.tsx": $6,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/api/seo.ts
@@ -0,0 +1,13 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+  const data: Record<string, unknown> = {
+    description:
+      "he asset management division of JP Morgan Chase &amp; Co., offers a wide range of mutual funds spanning all asset classes, adhering to distinct investment styles, to build portfolios to suit various investor goals and objectives.",
+    keyword: "asset managements funds",
+  };
+
+  const body = JSON.stringify(data);
+
+  return new Response(body);
+};
