731df79adaf8e06c50d6602e5a44ec984b7f5a5b
gary <garyhe.05@outlook.com>
2023-03-03 11:05:03 +0800

    fix: cors

--- routes/api/contact-us.ts
+++ routes/api/contact-us.ts
@@ -1,11 +1,19 @@
 import { HandlerContext } from "$fresh/server.ts";
 import { createMessage } from "../../models/message.ts";
+const corsInfo = {
+  headers: {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+  },
+};
 
 export const handler = {
   async POST(_req: Request, _ctx: HandlerContext) {
     const payload: Record<string, unknown> = await _req.json();
+
     if (!payload["name"] || !payload["email"] || !payload["message"]) {
-      return new Response("error");
+      return new Response("error", corsInfo);
     }
 
     const bool = await createMessage({
@@ -14,6 +22,6 @@ export const handler = {
       message: payload["message"] as string,
     });
 
-    return new Response(bool ? "OK" : "error");
+    return new Response(bool ? "OK" : "error", corsInfo);
   },
 };
